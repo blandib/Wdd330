c770f8a0c658df183ce205ad877dcad3591ea0a5
miguelramirez26 <26ramirez.m@gmail.com>
2024-01-15 14:56:01 +0100

    fix add to cart bug

--- src/js/cart.js
+++ src/js/cart.js
@@ -3,7 +3,8 @@ import { getLocalStorage } from "./utils.mjs";
 
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
+  const cartItems = getLocalStorage("so-cart") || [];
+  console.debug(cartItems);
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
