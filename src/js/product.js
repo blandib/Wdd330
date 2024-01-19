f5ceaa0770524a657f44ad3144f631d618d49ba2
ProudNamwera4 <proudnamwera4@gmail.com>
2024-01-19 06:51:46 +0200

    dynamic product details page

--- src/js/product.js
+++ src/js/product.js
@@ -3,7 +3,6 @@ import { getParam } from "./utils.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
-
 const productId = getParam("product");
 
 const product = new ProductDetails(productId, dataSource);
