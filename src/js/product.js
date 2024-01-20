a8e9ee7098171f216f6dcf48c30f62e5dfe5eefa
Hatsumi07 <crystalhatsumi07@gmail.com>
2024-01-20 16:30:42 -0500

    changes for team activity 2

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,22 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
 
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
+const productId = getParam("id");
+dataSource
+    .findProductById(productId)
+    .then(product => console.log(product)
+    );
+
+const product = new ProductDetails(productId, dataSource);
+product.init();
+
+// function addProductToCart(product) {
+//   setLocalStorage("so-cart", product);
+// }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
