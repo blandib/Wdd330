46075ec9aee81b83e3724623487953c7aa5532fd
ProudNamwera4 <proudnamwera4@gmail.com>
2024-01-21 05:09:59 +0200

    Dynamic Product List

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,8 @@
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
 
-import {productData} from "./ProductData.mjs";
-import { ProductListing } from "./ProductList.mjs";
+const dataSource = new ProductData("tents");
+const element = document.querySelector(".product-list");
+const listing = new ProductList("Tents", dataSource, element);
 
-const productData = new productData();
-class ProductListing {
-    constructor(products) {
-        this.products = products;
-    }
-
-    displayProducts() {
-        this.products.forEach(product => {
-            console.log(product);
-        });
-    }
-}
-
-// Create an array of products
-let products = ["Products.Brand.Name", "Products.Name", "Product.Price"];
-
-// Create an instance of ProductListing
-let productListing = new ProductListing(products);
-
-// Display the list of products
-productListing.displayProducts();
+listing.init();
