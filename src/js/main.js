3352b6274fa187ebfe0d900e85dce4fd1e47321c
ProudNamwera4 <proudnamwera4@gmail.com>
2024-01-26 07:06:19 +0200

    Dynamic header and footer,stretch activity attempt

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,15 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
+import {loadHeaderFooter} from "./utils.mjs";
+
 
 const dataSource = new ProductData("tents");
 const element = document.querySelector(".product-list");
 const listing = new ProductList("Tents", dataSource, element);
 
 listing.init();
+
+loadHeaderFooter("src/index.html");
+loadHeaderFooter("product_pages/index.html");
+loadHeaderFooter("cart/index.html");
+loadHeaderFooter("cart/checkout.html");
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -2,8 +2,6 @@
 export function qs(selector, parent = document) {
   return parent.querySelector(selector);
 }
-// or a more concise version if you are into that sort of thing:
-// export const qs = (selector, parent = document) => parent.querySelector(selector);
 
 // retrieve data from localstorage
 export function getLocalStorage(key) {
@@ -30,6 +28,8 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
+
+// function to take a list of objects and a template and insert the objects as HTML into the DOM
 export function renderListWithTemplate(
   templateFn,
   parentElement,
@@ -44,3 +44,40 @@ export function renderListWithTemplate(
   }
   parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
 }
+
+//load the templates
+export function renderWithTemplate(
+  templateFn,
+  parentElement,
+  data,
+  position = "afterbegin",
+  clear = false
+) {
+  parentElement.insertAdjacentHTML("afterbegin", templateFn);
+  // if clear is true we need to clear out the contents of the parent.
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
+
+//Load,Grab and Render the header and footer 
+export async function loadTemplate(path) {
+  const html = await fetch(path).then(convertToText);
+  const template = document.createElement('template');
+  template.innerHTML = html;
+  return template;
+}
+
+export async function loadHeaderFooter(path) {
+  const template = loadTemplate(path)
+  const header = document.querySelector("header");
+  const footer = document.querySelector("footer");
+  renderWithTemplate();
+}
+
+
+
+/*const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const product = urlParams.get('product')*/
