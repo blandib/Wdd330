fc933ee76d8b3d3efc56c8e19e3a8e50ac06fdc7
Brother Nelson <nelsoned@byui.edu>
2024-01-25 19:22:48 -0700

    Various fixes

--- src/js/product.js
+++ src/js/product.js
@@ -1,5 +1,7 @@
 import ProductData from "./ProductData.mjs";
-import { setLocalStorage, getParam } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
+import ProductDetails from "./ProductDetails.mjs";
+
 const dataSource = new ProductData("tents");
 
 const productId = getParam("product");
