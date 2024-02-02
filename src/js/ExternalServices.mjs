159f19388595438e977704a2dc4eef076dad2383
Blandine <blandinebukasa118@gmail.com>
2024-02-02 11:04:48 +0200

    W04 Team Activity

---
+++ src/js/ExternalServices.mjs
@@ -0,0 +1,67 @@
+
+const baseURL = 'https://wdd330-backend.onrender.com/';
+
+async function convertToJson(res) {
+  const jsonResponse = await res.json();
+  if (res.ok) {
+    return jsonResponse;
+  } else {
+    throw { name: "servicesError", message: jsonResponse };
+  }
+}
+
+export default class ExternalServices {
+  constructor(category) {
+    // this.category = category;
+    // this.path = `../json/${this.category}.json`;
+  }
+  async getData(category) {
+    const response = await fetch(baseURL + `products/search/${category}`);
+    const data = await convertToJson(response);
+    return data.Result;
+  }
+  async findProductById(id) {
+    const response = await fetch(baseURL + `product/${id}`);
+    const data = await convertToJson(response);
+    return data.Result;
+  }
+  async checkout(payload) {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    };
+    return await fetch(baseURL + "checkout/", options).then(convertToJson);
+  }
+  async loginRequest(user) {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    };
+    const response = await fetch(baseURL + "login", options).then(
+      convertToJson
+    );
+    return response.accessToken;
+  }
+  // make a request to the server for the current orders
+  // requires: a valid token
+  // returns: a list of orders
+  async getOrders(token) {
+    const options = {
+      method: "GET",
+      // the server will reject our request if we don't include the Authorization header with a valid token!
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const response = await fetch(baseURL + "orders", options).then(
+      convertToJson
+    );
+    return response;
+  }
+}
