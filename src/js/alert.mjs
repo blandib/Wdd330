8912998b460626ada50d53193f3ec89523dc4b7b
Hatsumi07 <crystalhatsumi07@gmail.com>
2024-02-24 07:28:37 -0500

    added customizable alert to index.html

---
+++ src/js/alert.mjs
@@ -0,0 +1,29 @@
+import { renderListWithTemplate } from "./utils.mjs";
+
+function alertItem(alert) {
+    return `<p style="background:${alert.background}; 
+    color:${alert.color};">${alert.message}</p>`;
+}
+
+export default class Alert {
+    constructor() {
+        this.alert = fetch("../public/json/alerts.json");
+    }
+    async init() {
+        const alert = await this.alert;
+        const data = await alert.json();
+        this.displayAlert(data);
+    }
+    displayAlert(data) {
+        const parentElement = document.querySelector("main");
+        console.log("parent element", parentElement);
+        if(data.length === 0) {
+            console.log("json file empty");
+        } else {
+            const alertList = document.createElement("section");
+            alertList.setAttribute("class", "alert-list");
+            parentElement.appendChild(alertList);
+            renderListWithTemplate(alertItem, alertList, data);
+        }
+    }
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
 import { loadHeaderFooter } from "./utils.mjs";
+import Alert from "./alert.mjs";
 
 loadHeaderFooter();
+const alert = new Alert;
+alert.init();
